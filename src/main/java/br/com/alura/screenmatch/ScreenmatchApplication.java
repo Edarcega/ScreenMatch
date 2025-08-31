bcdadb946801472f3511fa55b00052d1be47a74a
edimar <edimar.eds@gmail.com>
2025-08-30 21:04:07 -0300

    Feat: Implementando conversão de dados usando generics

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +21,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 //		System.out.println(consulta);
 //		consulta = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(consulta);
-
+		ConverteDados converteDados = new ConverteDados();
+		DadosSerie dadosSerie = converteDados.obterDados(consulta, DadosSerie.class);
+		System.out.println(dadosSerie);
 	}
 }
